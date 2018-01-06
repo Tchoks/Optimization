75498f649c7157c201424c524c68bc88800af48d
MarLeo <martin.aurele12@gmail.com>
2018-01-07 00:39:27 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <ilcplex/ilocplex.h>
+
+ILOSTLBEGIN
+
+int main(int argc, char **argv) {
+
+
+    IloEnv env;
+    IloModel model(env);
+    IloCplex cplex(model);
+
+    IloNumVar x1(env, 0.0, 40.0, ILOFLOAT, "X1");
+    IloNumVar x2(env, -10, 20, ILOINT, "X2");
+
+    model.add(x1);
+    model.add(x2);
+
+    IloObjective obj(env, x1 + x2, IloObjective::Minimize, "OBJ");
+    model.add(obj);
+
+    IloRange ctr1(env, 2, 2.5 * x1 + x2, IloInfinity, "C1");
+    IloRange ctr2(env, -IloInfinity, x1 - 3 * x2, 10, "C2");
+    model.add(ctr1);
+    model.add(ctr2);
+
+    cplex.exportModel("test.lp");
+    cplex.solve();
+    cplex.writeSolution("sol.txt");
+
+    std::cout << " Objectif = " << cplex.getObjValue() << std::endl;
+    std::cout << " X1 = " << cplex.getValue(x1) << std::endl;
+    std::cout << " X2 = " << cplex.getValue(x2) << std::endl;
+
+    env.end();
+
+    return 0;
+}
